a1802bf278adbe8526e9542aa0bebe92ba860f91
M Tavassoli <m.tavassoli.nor@gmail.com>
2022-12-12 00:21:51 +0100

    Major Changes: 	- added seeded random number 	- changed the logic of time measurement 	- changed the way of string generator

--- experiments/API.js
+++ experiments/API.js
@@ -1,13 +1,16 @@
 import Stopwatch from "./Stopwatch.js";
+import seedrandom from "seedrandom";
+import fs from "fs";
 
 export default class API {
   constructor(Ipfs, OrbitDB) {
     this.Ipfs = Ipfs;
     this.OrbitDB = OrbitDB;
+    this.start = 0;
+    this.end = 0;
+    this.put_duration = 0;
+    this.prefix = "key";
   }
-
-  prefix = "key";
-  stopWatch = new Stopwatch();
 
   async create() {
     this.node = await this.Ipfs.create({
@@ -38,40 +41,51 @@ export default class API {
     this.onready();
   }
 
+  // put key-value pairs into the database
   async put(keyNumbers, valueSize) {
     let rand_string = "";
-    this.stopWatch.start();
-
     for (let i = 0; i < keyNumbers; i++) {
       rand_string = randStr(valueSize);
+      console.log(`{key: ${this.prefix}-${i}, value: ${rand_string}}`);
+      this.start = performance.now();
       await this.exp.put(`${this.prefix}-${i}`, rand_string);
+      this.end = performance.now();
+      this.put_duration = this.put_duration + (this.end - this.start);
     }
-
-    const stop = this.stopWatch.stop();
-    console.log("stop: ", stop);
-    this.stopWatch.reset();
+    console.log(`duration: ${this.put_duration}`);
+    this.writeToFile();
   }
 
+  // get a value from the database // TODO
   async get() {
-    const key = `${this.prefix}-${Math.floor(Math.random() * 10)}`;
-    console.log("key: ", key);
-    const value = await this.exp.get(key);
+    const value = await this.exp.get("key-0");
     return value;
+  }
+
+  // write the time measurement to a csv file
+  writeToFile() {
+    const date = new Date();
+    const data = `
+      id, put_duration, get_duration
+       ${date.getTime()} ,${this.put_duration}, 0
+    `;
+    fs.appendFile("timeMeasurement.csv", data, (err) => {
+      if (err) throw err;
+      console.log("Saved!");
+    });
   }
 }
 
+// generate random string
 const randStr = (valueSize) => {
-  const stringSample = [
-    "OrbitDb is a serverless database.",
-    "OrbitDB uses IPFS.",
-    "OrbitDB is an excellent choice for decentralized apps.",
-    "uses CRDTs for conflict-free database merges.",
-  ];
-  const generatedString = [];
-  for (let i = 0; i < valueSize; i++) {
-    generatedString.push(
-      stringSample[Math.floor(Math.random() * stringSample.length)]
-    );
+  const chars =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+  const charLength = chars.length;
+  let result = "";
+  // Seeded random number generator
+  const rng = seedrandom();
+  for (var i = 0; i < valueSize; i++) {
+    result += chars.charAt(Math.floor(rng() * charLength));
   }
-  return generatedString.join(" ");
+  return result;
 };
--- experiments/index.js
+++ experiments/index.js
@@ -5,7 +5,6 @@ import * as Ipfs from "ipfs";
 const main = async () => {
   let keyNumbers = "";
   let valueSize = "";
-  let totalSize = "";
   if (process.argv.slice(2).length === 0) {
     console.log(
       `Wrong number of arguments, expected 2 arguments, 
@@ -16,13 +15,14 @@ const main = async () => {
   } else {
     keyNumbers = process.argv.slice(2)[0];
     valueSize = process.argv.slice(2)[1];
-    totalSize = keyNumbers * valueSize;
     console.log(`keyNumbers: ${keyNumbers}, valueSize: ${valueSize}`);
   }
   try {
     const api = new API(Ipfs, OrbitDB);
     api.onready = async () => {
       await api.put(keyNumbers, valueSize);
+      // const value = await api.get();
+      // console.log(`value: ${value}`);
     };
 
     await api.create();
